16b2205b20afe9dfdd52d190c9d4d88939bfcca4
Steve Holden <steve@holdenweb.com>
2019-03-12 22:08:16 +0000

    Use sets of tuples to make tests order-independent. Add a larger test against itertools.permutations. Add code to show the itertools speed advantage when run.

--- perms.py
+++ perms.py
@@ -1,8 +1,8 @@
 def process(stem, alphabet):
-    """outputs (as lists) all permutations of a set of symbols in alphabet
-        stem should be supplied as an empty list"""
+    """Produces (as tuples) all permutations of a set of symbols
+       from an alphabet stem should be supplied as an empty list"""
     if not alphabet: #all symbols already in stem
-        yield stem
+        yield tuple(stem)
     else:
         acopy = set(alphabet) # local copy
         while acopy:
@@ -23,18 +23,25 @@ def process(stem, alphabet):
 #
 import pytest
 from itertools import permutations
+from timeit import timeit
 
 def test_empty():
-    assert [x for x in process([], [])] == [[]]
+    assert {tuple(x) for x in process([], [])} =={()}
 
 def test_one_element():
-    assert [x for x in process([], {1})] == [[1]]
+    assert {tuple(x) for x in process([], {1})} == {(1, )}
 
 def test_two_elements():
-    assert [x for x in process([], {1, 2})] == [[1, 2], [2, 1]]
+    assert {x for x in process([], {1, 2})} == {(1, 2), (2, 1)}
 
 def test_three_elements():
-    assert [x for x in process([], {1, 2, 3})] == [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    assert {x for x in process([], {1, 2, 3})} == {(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)}
 
-def test_ten_elements():
-    assert set([tuple(x) for x in process([], set(range(8)))]) == set((x for x in permutations(range(8))))
+def test_nine_elements():
+    assert {tuple(x) for x in process([], set(range(9)))} == {x for x in permutations(range(9))}
+
+if __name__ == '__main__':
+    t1 = timeit("{tuple(x) for x in process([], set(range(5)))}", "from __main__ import process", number=100)
+    t2 = timeit("{x for x in permutations(range(5))}", "from __main__ import process, permutations", number=100)
+
+    print(f"process: {t1:.5f} permutations: {t2:.5f} faster by: {t1/t2:.1f} times")
